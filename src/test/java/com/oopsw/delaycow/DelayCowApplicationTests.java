770a7466fc522632c88e3776a822d8c4c113b4b3
wldusdn <wldusdngg@naver.com>
2025-11-03 14:04:13 +0900

    [KAN-36] :tada: init: Springboot setting

---
+++ src/test/java/com/oopsw/delaycow/DelayCowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopsw.delaycow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelayCowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
