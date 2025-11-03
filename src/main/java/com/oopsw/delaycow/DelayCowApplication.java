6df66b01e0fe59648442a932d20a734840b70157
wldusdn <wldusdngg@naver.com>
2025-11-03 14:04:13 +0900

    [Jira KAN-36] :tada: init: Springboot setting

---
+++ src/main/java/com/oopsw/delaycow/DelayCowApplication.java
@@ -0,0 +1,13 @@
+package com.oopsw.delaycow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelayCowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelayCowApplication.class, args);
+    }
+
+}
